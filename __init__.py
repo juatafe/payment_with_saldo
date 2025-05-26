974c0235cc92a7fa08489e84a4f91e5ac72fc59b
Juan Bautista Talens Felis <43112835+juatafe@users.noreply.github.com>
2025-05-26 11:24:51 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
+# payment_with_saldo/__init__.py
+
+# Import other parts of your module
 from . import models
 from . import controllers
-from . import hooks  # Això és correcte
-
-def post_load_hook(cr, registry):
-    from .hooks import archive_default_shop
-    archive_default_shop(cr, registry)
+from .hooks import archive_default_shop # Make sure this line exists to import your hooks module
